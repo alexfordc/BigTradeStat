2e80ca925afceebe17e23f46ab5cab8e0e4b910d
jchxu <jchxu@qq.com>
2018-11-01 15:23:41 +0800

    5

--- BigTradeStat_v2.py
+++ BigTradeStat_v2.py
@@ -7,6 +7,8 @@ from OperFunc import *
 from ReadFunc import *
 from WriteFunc import *
 import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 #####################
 #codelist = ['RB.SHF','I.DCE','J.DCE']
@@ -20,138 +22,92 @@ bigline = 1000     #手动指定大单标准线
 total = []
 total_nature = []
 now = datetime.now().strftime('%Y%m%d')
-outfile = open('test_' + now + '.csv', mode='w')
-output_title(outfile)
-
-start = datetime.now()
-print('开始：',start.strftime('%Y-%m-%d %H:%M:%S'))
-
-oldfiles, livefile = get_files(code)        #默认当前日期或最后一个文件为正在更新的数据文件
-if len(oldfiles) > 0:
-    olddf = read_old_files(oldfiles)
-    print(olddf)
-    end1 = datetime.now()
-    print('读取以往文件结束：',end1.strftime('%Y-%m-%d %H:%M:%S'))
-    print('读取以往文件用时：',(end1-start).seconds,'秒')
-
-    ### 读取以往数据，逐行计算各参量 ###
-    for index, row in olddf.iterrows():
-        rowdate = row.rt_date
-        rowtime = row.rt_time
-        rowlastpx = row.rt_last
-        df = olddf[:index+1]        #切片获取到index为止的所有数据
-        dfbig = olddf[:index+1].loc[df['rt_last_vol'] >= bigline]       #切片获取到index为止的大单数据
-        total = sum_big_vol(df, dfbig)      #累积的总交易量、总增仓量、大单交易量、大单增仓量
-        total_nature = split_big_group(df, dfbig)       #按性质分组的，累积总交易量、总增仓量、大单交易量、大单增仓量
-        print(rowdate,rowtime,rowlastpx,total)
-        output(outfile,rowdate,rowtime,rowlastpx,total,total_nature)
-    end2 = datetime.now()
-    print('统计以往数据结束：',end2.strftime('%Y-%m-%d %H:%M:%S'))
-    print('统计以往数据用时：',(end2-end1).seconds,'秒')
-else:
-    print('无以往数据 或 仅有一个数据文件')
-
-livefile = '../GetDayTradeInfo/RB.SHF_20181029.csv'
-
-print(total,total_nature)
-
-num = 0
-iii = 0
-while iii < 5:
-    livedf = read_live_file(livefile,num)
-    #num += 5
-    iii += 1
-    print('-'*20)
-    num += len(livedf)
-    if len(livedf) > 0:
-        for index, row in livedf.iterrows():
-            rowdate = row.rt_date
-            rowtime = row.rt_time
-            rowlastpx = row.rt_last
-            df = livedf[:index+1]        #切片获取到index为止的所有数据
-            dfbig = livedf[:index+1].loc[df['rt_last_vol'] >= bigline]       #切片获取到index为止的大单数据
-            live_total = live_sum_big_vol(df, dfbig,total)      #累积的总交易量、总增仓量、大单交易量、大单增仓量
-            live_total_nature = live_split_big_group(df, dfbig,total_nature)       #按性质分组的，累积总交易量、总增仓量、大单交易量、大单增仓量
-            print(rowdate,rowtime,rowlastpx,live_total)
-            output(outfile,rowdate,rowtime,rowlastpx,live_total,live_total_nature)
-        total = live_total
-        total_nature = live_total_nature
-        print(total, total_nature)
-
-    end3 = datetime.now()
-    print('统计实时数据结束：', end3.strftime('%Y-%m-%d %H:%M:%S'))
-    print('统计实时数据用时：', (end3 - end2).seconds, '秒')
-    time.sleep(10)
-
-print('总结束：',end3.strftime('%Y-%m-%d %H:%M:%S'))
-print('总用时：',(end3-start).seconds,'秒')
-
-
-
-#output_title()
-######################
+filename = 'test_' + now + '.csv'
+#outfile = open(filename, mode='w')
+#output_title(outfile)
 #
-#df = ReadTradeInfo(code)    #读取之前的交易数据,{index:dataframe}
-##print(df.rt_nature)
+#start = datetime.now()
+#print('开始：',start.strftime('%Y-%m-%d %H:%M:%S'))
 #
-#for index, row in df.iterrows():
-#    if index >= 10230:
+#oldfiles, livefile = get_files(code)        #默认当前日期或最后一个文件为正在更新的数据文件
+#
+#if len(oldfiles) > 0:
+#    olddf = read_old_files(oldfiles)
+#    #print(olddf)
+#    end1 = datetime.now()
+#    print('读取以往文件结束：',end1.strftime('%Y-%m-%d %H:%M:%S'))
+#    print('读取以往文件用时：',(end1-start).seconds,'秒')
+#
+#    ### 读取以往数据，逐行计算各参量 ###
+#    for index, row in olddf.iterrows():
 #        rowdate = row.rt_date
 #        rowtime = row.rt_time
-#        df = df[:index+1]  #切片获取到index为止的数据
-#        dfbig = df[:index+1].loc[df['rt_last_vol'] >= bigline]
-#        #print(df)
-#        #print(dfbig)
-#        total_vol = df.iloc[:,4].sum()     #到index为止的总交易量
-#        total_oi = df.iloc[:,5].sum()      #到index为止的总增仓量
-#        big_vol = dfbig.iloc[:,4].sum()     #到index为止的大单总交易量
-#        big_oi = dfbig.iloc[:,5].sum()      #到index为止的大单总增仓量
+#        rowlastpx = row.rt_last
+#        df = olddf[:index+1]        #切片获取到index为止的所有数据
+#        dfbig = olddf[:index+1].loc[df['rt_last_vol'] >= bigline]       #切片获取到index为止的大单数据
+#        total = sum_big_vol(df, dfbig)      #累积的总交易量、总增仓量、大单交易量、大单增仓量
+#        total_nature = split_big_group(df, dfbig)       #按性质分组的，累积总交易量、总增仓量、大单交易量、大单增仓量
+#        print(rowdate,rowtime,rowlastpx,total)
+#        output(outfile,rowdate,rowtime,rowlastpx,total,total_nature)
+#    end2 = datetime.now()
+#    print('统计以往数据结束：',end2.strftime('%Y-%m-%d %H:%M:%S'))
+#    print('统计以往数据用时：',(end2-end1).seconds,'秒')
+#else:
+#    print('无以往数据 或 仅有一个数据文件')
 #
-#        vol_nature, oi_nature, big_vol_nature, big_oi_nature = split_big_group(df, dfbig)
+##livefile = '../GetDayTradeInfo/RB.SHF_20181029.csv'
 #
-#        output(rowdate,rowtime,total_vol,total_oi,big_vol,big_oi,vol_nature, oi_nature, big_vol_nature, big_oi_nature)
-
-        #print(grouped.loc[grouped['rt_nature']==1,'rt_last_vol'])
-#column_vol_sum = df[code].iloc[:,4].sum()
-#print(code,column_vol_sum)
-#df2[code] = df[code].loc[df[code]['rt_last_vol'] > bigline]
-
-#grouped = df2[code].groupby('rt_nature').sum()
-#print(grouped)
-#amt1 = grouped.ix[1].rt_last_vol
-#print(amt1)
-#print(amt1/column_vol_sum)
-
-
-
-#codes = ','.join(codelist)
-#rt_date = {}
-#rt_time = {}
-#rt_last = {}
-#rt_last_vol = {}
-#rt_nature = {}
-#codefile = {}
+##print(total,total_nature)
 #
-#for item in codelist:
-#    rt_date[item] = ''
-#    rt_time[item] = ''
-#    rt_last[item] = ''
-#    rt_last_vol[item] = ''
-#    rt_nature[item] = 0
-    #filename = item+".csv"
-    #if os.path.isfile(filename):
-    #    codefile[item] = open(filename, 'a')
-    #else:
-    #    codefile[item] = open(filename, 'a')
-    #    print('rt_date','rt_time','rt_last','rt_last_vol','rt_nature',sep=',',file=codefile[item])
-    #    codefile[item].flush()
-#def test(indata):
-    #print(indata)
-    #return(indata.Codes)
+#num = 0
+#iii = 0
+#while iii < 5:
+#    start2 = datetime.now()
+#    livedf = read_live_file(livefile,num)
+#    #num += 5
+#    iii += 1
+#    print('-'*20)
+#    num += len(livedf)
+#    if len(livedf) > 0:
+#        for index, row in livedf.iterrows():
+#            rowdate = row.rt_date
+#            rowtime = row.rt_time
+#            rowlastpx = row.rt_last
+#            df = livedf[:index+1]        #切片获取到index为止的所有数据
+#            dfbig = livedf[:index+1].loc[df['rt_last_vol'] >= bigline]       #切片获取到index为止的大单数据
+#            live_total = live_sum_big_vol(df, dfbig,total)      #累积的总交易量、总增仓量、大单交易量、大单增仓量
+#            live_total_nature = live_split_big_group(df, dfbig,total_nature)       #按性质分组的，累积总交易量、总增仓量、大单交易量、大单增仓量
+#            print(rowdate,rowtime,rowlastpx,live_total)
+#            output(outfile,rowdate,rowtime,rowlastpx,live_total,live_total_nature)
+#        total = live_total
+#        total_nature = live_total_nature
+#        #print(total, total_nature)
+#
+#    end3 = datetime.now()
+#    print('统计实时数据结束：', end3.strftime('%Y-%m-%d %H:%M:%S'))
+#    print('统计实时数据用时：', (end3 - start2).seconds, '秒')
+#    time.sleep(1)
+#outfile.close()
+#print('总结束：',end3.strftime('%Y-%m-%d %H:%M:%S'))
+#print('总用时：',(end3-start).seconds,'秒')
 
-#w.start()
-#tradeinfo = w.wsq(codes, "rt_date,rt_time,rt_last,rt_last_vol,rt_nature",func=test)
-#print(tradeinfo)
-    #实时绘图
-#w.stop()
-#  w.cancelRequest(0)
+
+rtdatetime = np.loadtxt(filename, delimiter=',', dtype=str, usecols=(1), unpack=False, skiprows=1)
+last_px,total_vol,total_oi,big_vol = np.loadtxt(filename, delimiter=',', dtype=int, usecols=(2,3,4,5), unpack=True, skiprows=1)
+total_vol_2,total_vol_4 = np.loadtxt(filename, delimiter=',', dtype=int, usecols=(11,19), unpack=True, skiprows=1)
+total_vol_1,total_vol_5 = np.loadtxt(filename, delimiter=',', dtype=int, usecols=(7,23), unpack=True, skiprows=1)
+total_vol_duo = total_vol_2 + total_vol_4
+total_vol_kong = total_vol_1 + total_vol_5
+
+plt.rcParams['figure.figsize'] = (20.0, 16.0)
+plt.rcParams['lines.linewidth'] = 1
+#plt.plot(rtdatetime,last_px)
+plt.xticks(rotation=90)
+plt.plot(rtdatetime,last_px, color='black', label='Live Price')
+plt.plot(rtdatetime,total_vol, color='black', label='Total Volumn')
+plt.plot(rtdatetime,total_oi, color='black', label='Total OI Change')
+plt.plot(rtdatetime,big_vol, color='black', label='Big Trade Volumn')
+plt.plot(rtdatetime,total_vol_duo, color='red', label='Duo Trade Volumn')
+plt.plot(rtdatetime,total_vol_kong, color='green', label='Kong Trade Volumn')
+plt.legend()
+plt.show()

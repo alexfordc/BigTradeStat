6644c25013562ada1cf7aa744d7b0354152b8a01
jchxu <jchxu@qq.com>
2019-01-10 17:23:30 +0800

    minute initial

--- Minute_DuoKong_AmtVol_Slope.py
+++ Minute_DuoKong_AmtVol_Slope.py
@@ -14,7 +14,12 @@ origin_path = '../Wind直接导出数据/'      #从Wind导出的原始数据的
 treated_path = 'Treated_Data/'          #保存增加持仓量、按性质分类后的数据的路径
 already_StatMinute_logfile = 'Already_StatMinute_Files.log'     #记录哪些数据文件已经统计了每分钟内的多空次量，价格变化
 #######################################################
-
+already_statminute_files = get_already_files(already_StatMinute_logfile)
 for i in range(len(codelist)):      #按照code列表中的顺序依次处理
     code = codelist[i]
-    treated_files = get_treated_files(code, already_StatMinute_logfile, treated_path)
+    treated_files = get_treated_files(code, already_statminute_files, treated_path)
+    treated_files = treated_files[0:1]
+    if len(treated_files) > 0:
+        for j in range(len(treated_files)):
+            treated_file = treated_files[j]
+            print(treated_file)
--- Minute_DuoKong_AmtVol_Slope_Func.py
+++ Minute_DuoKong_AmtVol_Slope_Func.py
@@ -16,7 +16,17 @@ def get_already_files(already_read_logfile):
         file.close()
     return already_read_files
 
-def get_treated_files(code, already_StatMinute_logfile, treated_path):
+def get_treated_files(code, already_statminute_files, treated_path):
     '''获取尚未进行每分钟统计的文件列表，返回文件名列表'''
     allfiles = os.listdir(treated_path)
-    already_statminute_files = get_already_files(already_StatMinute_logfile)
+    codefiles = []
+    for item in allfiles:
+        if (code in item) and (item not in already_statminute_files):
+            codefiles.append(item)
+    codefiles = sorted(codefiles)  # 按照文件名排序
+    print('#' * 100)  # 分割线
+    if len(codefiles) == 0:
+        print('所有%s文件均已按每分钟统计。如需重读，请更改已处理文件列表。' % (code))
+    else:
+        print('需统计%d个%s数据文件: %s' % (len(codefiles), code, ', '.join(codefiles)))
+    return codefiles
